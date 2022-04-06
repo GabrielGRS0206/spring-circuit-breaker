51d24f2964c9431b918cd942bbbc661922329f15
Gabriel Rocha <gabrielrocha0206@gmail.com>
2022-04-06 14:03:57 -0300

    refatoring response notifications clients

--- clients/src/main/java/br/com/clients/bff/model/dto/NotificationResponseDto.java
+++ clients/src/main/java/br/com/clients/bff/model/dto/NotificationResponseDto.java
@@ -1,11 +1,8 @@
 package br.com.clients.bff.model.dto;
 
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
-import br.com.clients.bff.model.vo.NotificationVO;
 import lombok.Data;
 
 @Data
@@ -14,5 +11,4 @@ public class NotificationResponseDto {
 	
 	private Long id;
 	private String message;
-	private List<NotificationVO> notifications;
 }
